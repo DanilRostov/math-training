c228f5d8eb2c01469132299ecc84da3e749ddfec
DanilRostov <daniil.rossy@gmail.com>
2018-01-08 23:01:29 +0300

    main commit

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="alert alert-secondary">
+		<h3>Хорошо считаешь?</h3>
+		<button class="btn btn-success" @click="$emit('onStart')">Конечно!</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+	.alert {
+		text-align: center;
+	}
+	h3, .btn {
+		margin: 30px 0;
+	}
+</style>
